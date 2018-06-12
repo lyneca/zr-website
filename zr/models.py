1cdefe58100111de360c2240800d5a48eff3fb96
Lyneca <atraguliauniliantauthr@gmail.com>
2018-06-12 18:55:02 +1000

    add templates and static files

--- zr/models.py
+++ zr/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question = models.TextField("Question")
+    answer = models.TextField("Answer")
+    answerer = models.TextField("Answerer")
+    submitter = models.TextField("Submitter")
+    datetime = models.DateTimeField("Date submitted", auto_now=True)

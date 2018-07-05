c4a78d54b50dffb7d098af5ca90ecdaa3485151d
Lyneca <atraguliauniliantauthr@gmail.com>
2018-07-06 01:21:49 +1000

    create new pages

--- zr/urls.py
+++ zr/urls.py
@@ -1,7 +1,10 @@
-from .views import *
+from .views import index, about, resources, qanda, contact
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about', about, name='about'),
+    path('resources', resources, name='resources'),
     path('qanda', qanda, name='qanda'),
+    path('contact', contact, name='contact'),
 ]
--- zr/views.py
+++ zr/views.py
@@ -6,8 +6,17 @@ from .models import Question
 def index(request):
     return render(request, "zr/index.html")
 
+def about(request):
+    return render(request, "zr/about.html")
+
+def resources(request):
+    return render(request, "zr/resources.html")
+
 def qanda(request):
     context = {
         "questions": Question.objects.all()
     }
     return render(request, "zr/qanda.html", context=context)
+
+def contact(request):
+    return render(request, "zr/contact.html")
